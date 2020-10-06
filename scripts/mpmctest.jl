8150ab372bd1c6824b0fa09faaeb5bea6ac7197a
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-10-06 14:02:36 +0100

    updates

--- scripts/mpmctest.jl
+++ scripts/mpmctest.jl
@@ -3,8 +3,9 @@ using Optim
 using Distributions
 using Plots
 using LinearAlgebra
+using Random
 
-plotlyjs()
+gr()
 
 include(srcdir("pop_mc.jl"))
 include(srcdir("gapis.jl"))
@@ -28,7 +29,7 @@ for i = 1:100
     end
 end
 
-plot(xsq, ysq, fv, st = :heatmap)
+plot(xsq, ysq, fv, st = :heatmap, size = (750, 500))
 
 pf1 = MvNormal
 pf2 = MvNormal
@@ -52,7 +53,7 @@ end
 mpc = stratified_resample(nv[1], nv[4])
 
 # plot!(nv[1][1, :], nv[1][2, :], st = :scatter, legend = false)
-plot!(mpc[1, :], mpc[2, :], st = :scatter, legend = false)
+# plot!(mpc[1, :], mpc[2, :], st = :scatter, legend = false)
 
 pfs = [pf1, pf2]
 locations = [[2.0, 2.0], [-2.0, -2.0]]
@@ -72,10 +73,20 @@ rmm = RWIS_draw_means(tgt, 2, 100, 2, init_mvs; n_adapts = 1000)
 
 # global nv = LAIS_step(tgt, pfs, 250, 2, fixed_params, init_mvs)
 # global rw = zeros(2, 500, 100)
+global rmf = zeros(2, 500, 100)
+global rsp = zeros(2, 500, 100)
+global rmw = zeros(500, 100)
 for t = 1:100
     # global rw[:,:,t] = RWIS_step(tgt, pfs, 250, 2, fixed_params, [rmm[1][t], rmm[2][t]])[1]
     global rw = RWIS_step(tgt, pfs, 250, 2, fixed_params, [rmm[1][t], rmm[2][t]])
+    rmf[:,:,t] = rw[1]
+    rmw[:,t] = rw[2]
+    rsp[:,:,t] = stratified_resample(rw[1], rw[2])
 end
 rsm = stratified_resample(rw[1], rw[2])
 
-plot!(rsm[1, :], rsm[2, :], st = :scatter, legend = false)
+clonk = reshape(rsp, (2, 500*100))
+# thinby = randsubseq(1:(500*100), 0.02)
+# cln = clonk[:, thinby]
+# plot!(rsm[1, :], rsm[2, :], st = :scatter, legend = false)
+plot!(clonk[1, :], clonk[2, :], st = :scatter, legend = false)
